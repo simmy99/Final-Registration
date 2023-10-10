9b4f03649f7fb1fde64e1726b2d28c22ab39ec6e
Simmy Sebastian <simmysebastian99@gmail.com>
2023-10-10 16:29:06 +0530

    UI and validations completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'regTask';
+  user: any = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+    firstName: '',
+    middleName: '',
+    lastName: '',
+    phoneNumber: ''
+  };
+  isRegistered: boolean = false;
+  registrationSuccessful: boolean = false;
+
+  @ViewChild('registrationForm', { static: false }) form!: NgForm;
+
+  constructor(private modalRef: BsModalRef) { }
+
+  ngAfterViewInit() {
+  }
+
+  passwordsMatchValidator(): boolean {
+    return this.user['password'] === this.user['confirmPassword'];
+  }
+
+  registerUser() {
+    this.isRegistered = false;
+
+    if (this.form.valid) {
+      if (this.passwordsMatchValidator()) {
+        this.isRegistered = true;
+        this.registrationSuccessful = true;
+  
+        // Close the modal after 3 seconds (3000 milliseconds)
+        setTimeout(() => {
+          this.modalRef.hide();
+        }, 3000);
+      } else {
+        this.form.controls['confirmPassword'].setErrors({ 'passwordMismatch': true });
+      }
+    }
+  }
 }
